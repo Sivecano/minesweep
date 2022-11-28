e451912b7028eb1f4f37764de2be51a8c3dd5695
Sivecano <Sivecano@gmail.com>
2022-11-28 03:12:35 +0100

    rendering and playing; refactoring to reduce hardcoding is needed. no win or loss states

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_mouse.h>
+#include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 #include <algorithm>
@@ -161,19 +163,87 @@ void gui()
     
     bool running = true;
 
+    field a(10, 10);
+    a.fill(8);
+ 
     while (running)
     {
         SDL_Event e;
         while (SDL_PollEvent(&e))
         {
+            int x,y;
+            uint8_t state;
             switch(e.type)
             {
+                case SDL_MOUSEBUTTONUP:
+                    x = e.button.x;
+                    y = e.button.y;
+                    state = e.button.button;
+                    std::cout << "Click " << int(state) << " at " << x << ", " << y << "\n";
+                    if (x < 0 || x > 32 * a.width || y < 20 || (y-20) > 32*a.height) continue;
+
+                    x /= 32;
+                    y -= 20;
+                    y /= 32;
+
+                    switch(state)
+                    {
+                        case 1:
+                            a.guess(x, y);
+                            break;
+                        case 3:
+                            if (a.status[x + a.width * y] == 0) a.status[x + a.width * y] = -1;
+                            else if (a.status[x + a.width * y] == -1) a.status[x + a.width * y] = 0;
+
+                            break;
+
+                    }
+                    break;
+
                 case SDL_QUIT:
                     running = false;
                     break;
             }
         }
 
+        SDL_SetRenderDrawColor(ren, 30, 30, 30, 255);
+        SDL_RenderClear(ren);
+        
+        SDL_Rect src = {0, 0, 16, 16};
+        SDL_Rect dst = {0, 20, 32, 32};
+
+        for (uint16_t n = 0; n < a.width * a.height; n++)
+        {
+            if (n % a.width == 0 && n != 0)
+            {
+                dst.x = 0;
+                dst.y += 32;
+            }
+
+            switch (a.status[n])
+            {
+                case -1:
+                    src.y = 0;
+                    break;
+                case 0:
+                    src.y = 16;
+                    break;
+                case 1:
+                    if (a.grid[n])
+                        src.y = 32;
+                    else 
+                        src.y = 48 + 16* a.count(n % a.width, n / a.width);
+            }
+
+            SDL_RenderCopy(ren, tex, &src, &dst);
+            
+            dst.x += 32;
+        }
+
+
+
+
+        SDL_RenderPresent(ren);
     }
 
 
@@ -183,6 +253,7 @@ void gui()
 
 int main()
 {
+    gui();
     return 0;
 }
 
